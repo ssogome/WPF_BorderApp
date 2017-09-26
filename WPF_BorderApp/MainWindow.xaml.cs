898d7833a48937412ed2d7c7b9bcc90ba13ebbae
Pap Sogome <ssogome@gmail.com>
2017-09-26 17:25:11 -0600

    Add project files.

---
+++ WPF_BorderApp/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_BorderApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+         private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+         {
+                        Color color = Color.FromRgb((byte)slColorR.Value, (byte)slColorG.Value, (byte)slColorB.Value);
+                        this.Background = new SolidColorBrush(color);
+         }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    pbStatus.Value++;
+            //    Thread.Sleep(100);
+            //}
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.DoWork += worker_DoWork;
+            worker.ProgressChanged += worker_ProgressChanged;
+
+            worker.RunWorkerAsync();
+        }
+
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                (sender as BackgroundWorker).ReportProgress(i);
+                Thread.Sleep(100);
+            }
+        }
+
+        void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbStatus.Value = e.ProgressPercentage;
+        }
+
+        private void ToNextWindow_Click(object sender, RoutedEventArgs e)
+        {
+            WebBrowserWindow wb = new WebBrowserWindow();
+            this.Hide();
+            wb.Show();
+        }
+    }
+}
